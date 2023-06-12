9cf144cdabbf862da9dd0442b400b46760f65554
Petr Svoboda <petr@jatys.cz>
2023-06-12 18:25:53 +0200

    Add support for reference hyperlinks (NOT FOR UPSTREAM)

--- Template/table_view/reference.php
+++ Template/table_view/reference.php
@@ -1,1 +1,10 @@
-<?= $task['reference'] ?>
+<?php
+$ref = $task['reference'];
+if (!empty($ref)) {
+	$id = explode('-', $ref, 2);
+	$id = $id[count($id)-1];
+	$link = sprintf(getenv('KANBOARD_TABLE_REFLINK_TEMPLATE'), $id);
+	$link = htmlspecialchars($link);
+	printf('<a href="%s" target="_blank" rel="noopener noreferrer">%s</a>', $link, $ref);
+}
+?>
