62dda7e48b7c7031b6ddc078822671173ac18996
Petr Svoboda <petr@jatys.cz>
2023-06-12 19:08:04 +0200

    Open task link in a new tab (NOT FOR UPSTREAM)

--- Template/table_view/title.php
+++ Template/table_view/title.php
@@ -1,1 +1,1 @@
-<span class="<?= $task['is_active'] == 0 ? 'status-closed' : '' ?>"><?= $this->url->link($this->text->e($task['title']), 'TaskViewController', 'show', array('task_id' => $task['id'])) ?></span>
+<span class="<?= $task['is_active'] == 0 ? 'status-closed' : '' ?>"><?= $this->url->link($this->text->e($task['title']), 'TaskViewController', 'show', array('task_id' => $task['id']), newTab: true) ?></span>
